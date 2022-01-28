931760ba03810e92c2bf4b8ace956083f1e09326
NikKho <nh@codenrock.com>
2022-01-28 18:05:52 +0300

    add scope

--- src/Traits/NotificationsReceiver.php
+++ src/Traits/NotificationsReceiver.php
@@ -6,6 +6,15 @@ use Khonik\Notifications\Models\Notification;
 
 trait NotificationsReceiver
 {
+    public function scopeReceiverOfNotification($query,$notification_id){
+        if(!$notification_id){
+            return $query;
+        }
+        return $query->whereHas("manualNotifications", function ($q) use ($notification_id) {
+            $q->where("notifications.id", $notification_id);
+        });
+    }
+
     public function manualNotifications()
     {
         return $this->belongsToMany(Notification::class);
